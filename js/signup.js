5862cb8d67d19e0e1c19a10e506ee58513b5f1bc
jlin218 <jlin218@uw.edu>
2014-10-23 10:12:13 -0700

    start

--- js/signup.js
+++ js/signup.js
@@ -2,3 +2,9 @@
     Signup Form Script
     This script will load the state select list and validate the form before submission
 */
+document.addEventListener('DOMContentLoaded', onReady);
+function onReady {
+    var option = document.createElement("Option");
+
+}
+var  = document.getElementById("");
